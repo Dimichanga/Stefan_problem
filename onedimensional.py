e7aeec504a7f1b152c92fb8f7fe0b0ac6a6b4126
Dimichanga <92356906+Dimichanga@users.noreply.github.com>
2022-05-17 23:12:02 +0300

    Add files via upload

--- onedimensional.py
+++ onedimensional.py
@@ -31,10 +31,9 @@ def onedimensional_Stefan (str1, str2, str3, onedimens, Left, times):
     for i in range(1,numb+1):
             if i < phase_index: 
                 u[i] = initcond
-            elif i == phase_index:
-                u[i] = 0
             else:
-                u[i]=0
+                u[i] = 0
+           
     
     book = openpyxl.Workbook()
     sheet = book.active
@@ -46,45 +45,43 @@ def onedimensional_Stefan (str1, str2, str3, onedimens, Left, times):
     while times.actual < times.duration:
 
         sheet.cell(row = count + 2, column = 1).value = times.actual
-        for i in range (phase_index+1):
+        for i in range (numb+1):
             sheet.cell(row = count+2, column = i+2).value = u[i]
         count += 1
         
         for i in range (numb+1):
             u_[i] = u[i]
 
-        if (phase_index < numb): 
-            phase_index += 1
         
         while (abs(times.step - times.prev_step) >= eps):
-            A = (- tau) / (h**2)
-            B = 1 + (2 * tau/(h**2))
-            C = A
-            u[0] = Left.get(times.actual)
-            u[phase_index] =0
-            gamma [1] = B
-            alpha[1] = -C / gamma[1]
-            beta[1] = (u_[1] - A*u[0]) / gamma[1]
-
-            for i in range (2,phase_index):
-                gamma[i] = B + A * alpha[i-1]
-                if (i != phase_index-1):
-                    beta[i] = (u_[i] - A*beta[i-1]) / gamma[i]
-                    alpha[i] = -C / gamma[i]
-                else:
-                    beta[i] = (u_[i] - C*u[phase_index] - A*beta[i-1]) / gamma[i]
-                    alpha[i] = 0
-        
-            u[phase_index-1] = beta[phase_index-1]
-            for i in range (phase_index-2,0,-1):
-                u[i] = alpha[i] * u[i+1] + beta[i]
-
             times.prev_step = times.step
             times.step = -((enthalpy / coef_therm_cond )*h + 0.5*(u[phase_index]-u_[phase_index])) / ((u[phase_index] - u[phase_index-1]) / h)
 
-            
+        A = (- tau) / (h**2)
+        B = 1 + (2 * tau/(h**2))
+        C = A
+        u[0] = Left.get(times.actual)
+        u[phase_index] =0
+        gamma [1] = B
+        alpha[1] = -C / gamma[1]
+        beta[1] = (u_[1] - A*u[0]) / gamma[1]
 
+        for i in range (2,phase_index):
+            gamma[i] = B + A * alpha[i-1]
+            if (i != phase_index-1):
+                beta[i] = (u_[i] - A*beta[i-1]) / gamma[i]
+                alpha[i] = -C / gamma[i]
+            else:
+                beta[i] = (u_[i] - C*u[phase_index] - A*beta[i-1]) / gamma[i]
+                alpha[i] = 0
+        
+        u[phase_index-1] = beta[phase_index-1]
+        for i in range (phase_index-2,0,-1):
+            u[i] = alpha[i] * u[i+1] + beta[i]
+            
         times.actual += times.step
+        if (phase_index < numb): 
+            phase_index += 1
 
     sheet.cell(row = count + 3, column = 1).value = times.actual
     for i in range(numb+1):
